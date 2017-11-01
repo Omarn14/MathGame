5ea3f05b9eb7980b52f6ce794c1372f6aa600e89
Omar Navarro <14omar1414@gmail.com>
2017-11-01 15:34:54 -0400

    Simple Addition Game

---
+++ MathGame/MathGame/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace MathGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.aLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.plusLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // doneButton
+            // 
+            this.doneButton.Enabled = false;
+            this.doneButton.Location = new System.Drawing.Point(64, 251);
+            this.doneButton.Name = "doneButton";
+            this.doneButton.Size = new System.Drawing.Size(865, 103);
+            this.doneButton.TabIndex = 0;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(64, 378);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(865, 103);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // answerBox
+            // 
+            this.answerBox.Location = new System.Drawing.Point(64, 202);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(865, 26);
+            this.answerBox.TabIndex = 2;
+            this.answerBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.answerBox_KeyDown);
+            // 
+            // aLabel
+            // 
+            this.aLabel.AutoSize = true;
+            this.aLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aLabel.Location = new System.Drawing.Point(57, 67);
+            this.aLabel.Name = "aLabel";
+            this.aLabel.Size = new System.Drawing.Size(35, 37);
+            this.aLabel.TabIndex = 3;
+            this.aLabel.Text = "0";
+            this.aLabel.Click += new System.EventHandler(this.aLabel_Click);
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bLabel.Location = new System.Drawing.Point(212, 67);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(35, 37);
+            this.bLabel.TabIndex = 4;
+            this.bLabel.Text = "0";
+            // 
+            // plusLabel
+            // 
+            this.plusLabel.AutoSize = true;
+            this.plusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusLabel.Location = new System.Drawing.Point(130, 67);
+            this.plusLabel.Name = "plusLabel";
+            this.plusLabel.Size = new System.Drawing.Size(36, 37);
+            this.plusLabel.TabIndex = 5;
+            this.plusLabel.Text = "+";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(57, 150);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 40);
+            this.timerLabel.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 527);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.plusLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.aLabel);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.doneButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void aLabel_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button doneButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Label aLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label plusLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MathGame/MathGame/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathGame
+{
+    public partial class Form1 : Form
+    {
+        public int timeLeft;
+        public int a;
+        public int b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timeLeft = 10;
+            timerLabel.Text = timeLeft.ToString();
+            Random r = new Random();
+            a = r.Next(10) + 1;
+            b = r.Next(10) + 1;
+            aLabel.Text = a.ToString();
+            bLabel.Text = b.ToString();
+
+            timer1.Start();
+
+            startButton.Enabled = false;
+            doneButton.Enabled = true;
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Stop();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                timerLabel.Text = timeLeft.ToString();
+            }
+
+            if(timeLeft == 0)
+            {
+                doneButton.Enabled = false;
+                startButton.Enabled = true;
+                timer1.Stop();
+                timerLabel.Text = "You ran out of time. Try again. ";
+            }
+        }
+
+        private void doneButton_Click(object sender, EventArgs e)
+        {
+            doneButton.Enabled = false;
+            startButton.Enabled = true;
+            timer1.Stop();
+
+            int c = a + b;
+
+            if (answerBox.Text == c.ToString())
+            {
+                timer1.Stop();
+                timerLabel.Text = "Congratulations. You are correct!!!";
+            }
+            else
+            {
+                timerLabel.Text = "Incorrect. Play again.";
+            }
+        }
+
+        private void answerBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                doneButton.Focus();
+            }
+        }
+    }
+}
